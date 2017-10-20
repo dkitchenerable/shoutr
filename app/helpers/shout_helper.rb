96bcc7b04549d00ff5aae8d08b97eca7f857dd02
dkitchenerable <kitchenerdavid@gmail.com>
2017-10-19 21:18:34 -0400

    liking and unliking shouts

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -3,4 +3,12 @@ module ShoutHelper
     email_digest = Digest::MD5.hexdigest(user.email)
     image_tag "//s.gravatar.com/avatar/#{email_digest}?s=80"
   end
+
+  def like_button(shout)
+    if current_user.liked?(shout)
+      link_to "Unlike", unlike_shout_path(shout), method: :delete
+    else
+      link_to "Like", like_shout_path(shout), method: :post
+    end
+  end
 end
